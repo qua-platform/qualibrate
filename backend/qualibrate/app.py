22f4e416132446684e49dc62718eb8e1ba28e327
Maxim V4S <maxim.d@v4scale.com>
2024-02-21 15:07:27 +0300

    feat: add cors middleware

--- backend/qualibrate/app.py
+++ backend/qualibrate/app.py
@@ -1,13 +1,23 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
 
 from qualibrate.api.__main__ import api_router
 from qualibrate.config import get_settings
 
 app = FastAPI(title="Qualibrate")
 
-app.include_router(api_router, prefix="/api/local")
+origins = ["http://localhost:8002", "http://localhost:8001"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router, prefix="/api/json_db")
 
 # Directory should exist
 app.mount(
