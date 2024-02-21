a8429b768744a45887650998a833a44c326a2864
Maxim V4S <maxim.d@v4scale.com>
2024-02-21 15:33:37 +0300

    remove useless part

--- backend/qualibrate/api/__main__.py
+++ backend/qualibrate/api/__main__.py
@@ -1,8 +1,5 @@
 from fastapi import APIRouter
 
-from qualibrate.api.routes.snapshot import snapshot_router
-from qualibrate.api.routes.timeline import timeline_router
-
 
 api_router = APIRouter()
 
@@ -10,7 +7,3 @@ api_router = APIRouter()
 @api_router.get("/")
 def ping() -> str:
     return "pong"
-
-
-api_router.include_router(timeline_router)
-api_router.include_router(snapshot_router)
