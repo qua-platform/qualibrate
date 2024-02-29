9a16a68619aae95409a0096b23ea11260ff3f3a0
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-02-29 21:06:55 +0100

    overwrote number of snapshots to 200

--- frontend/src/utils/api/apiRoutes.ts
+++ frontend/src/utils/api/apiRoutes.ts
@@ -2,7 +2,7 @@ export const LOGIN = "auth/login";
 export const LOGOUT = "auth/logout";
 export const AUTH_VERIFY = "auth/verify";
 export const AUTH_INFO = "auth/user_info";
-export const ALL_SNAPSHOTS = (branchName: string = "main", reverseOrder = true) =>
-  "api/json_db/branch/" + branchName + "/history?reverse=" + reverseOrder;
+export const ALL_SNAPSHOTS = (branchName: string = "main", reverseOrder = true, num_snapshots = 200) =>
+    "api/json_db/branch/" + branchName + "/history?reverse=" + reverseOrder + "&num_snapshots=" + num_snapshots;
 export const ONE_SNAPSHOT = (snapshotId: string) => `api/json_db/snapshot/${snapshotId}/`;
 export const SNAPSHOT_RESULT = (snapshotId: string) => `api/json_db/storage/${snapshotId}/content`;
