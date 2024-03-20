6c8ef130c024c0be00ad1dc5cacc760fe8793bd9
Maxim V4S <maxim.d@v4scale.com>
2024-03-20 18:31:45 +0300

    refactor: update fastapi exception handler middleware

--- backend/qualibrate/api/exceptions/middleware.py
+++ backend/qualibrate/api/exceptions/middleware.py
@@ -10,19 +10,18 @@ from starlette.middleware.base import (
 from qualibrate.api.exceptions.classes.base import QualibrateException
 
 
-async def add_process_time_header(
-    request: Request, call_next: RequestResponseEndpoint
-) -> Response:
-    try:
-        return await call_next(request)
-    except QualibrateException as exc:
-        return JSONResponse(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            content={"detail": str(exc)},
-            headers={"X-Exception-Type": exc.__class__.__name__},
-        )
-
-
 class QualibrateCatchExcMiddleware(BaseHTTPMiddleware):
     def __init__(self, app: ASGIApp):
-        super().__init__(app, dispatch=add_process_time_header)
+        super().__init__(app)
+
+    async def dispatch(
+        self, request: Request, call_next: RequestResponseEndpoint
+    ) -> Response:
+        try:
+            return await call_next(request)
+        except QualibrateException as exc:
+            return JSONResponse(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                content={"detail": str(exc)},
+                headers={"X-Exception-Type": exc.__class__.__name__},
+            )
