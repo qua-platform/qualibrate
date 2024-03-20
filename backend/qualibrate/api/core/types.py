e870a3ce0fbb35abb9955d869e87944c6d96c087
Maxim V4S <maxim.d@v4scale.com>
2024-03-20 18:31:02 +0300

    fix: update id type

--- backend/qualibrate/api/core/types.py
+++ backend/qualibrate/api/core/types.py
@@ -1,8 +1,8 @@
 import string
-from typing import Union, Mapping, Any, Sequence
+from typing import Mapping, Any, Sequence
 
 AllowedSearchKeys = string.ascii_letters + string.digits + "-_*"
 
-IdType = Union[str, int]
+IdType = int
 DocumentType = Mapping[str, Any]
 DocumentSequenceType = Sequence[DocumentType]
