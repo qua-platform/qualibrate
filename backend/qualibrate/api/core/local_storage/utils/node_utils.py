80b7ceb581d45f44fe04c7a509f307bbdea678b9
Maxim V4S <maxim.d@v4scale.com>
2024-03-25 13:03:08 +0300

    feat: add optional filters for searching latest nodes

--- backend/qualibrate/api/core/local_storage/utils/node_utils.py
+++ backend/qualibrate/api/core/local_storage/utils/node_utils.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from pathlib import Path
-from typing import Generator
+from typing import Callable, Generator, Optional, Sequence
 
 from qualibrate.api.core.types import IdType
 
@@ -17,29 +17,42 @@ def find_latest_node_id(base_path: Path) -> IdType:
 
 
 def find_n_latest_nodes_ids(
-    base_path: Path, n: int
+    base_path: Path,
+    n: int,
+    date_filters: Optional[Sequence[Callable[[Path], bool]]] = None,
+    node_filters: Optional[Sequence[Callable[[Path], bool]]] = None,
 ) -> Generator[IdType, None, None]:
     """
     Generator of n latest nodes ids
     """
 
-    def _validate_date(date_str: str) -> bool:
+    def _validate_date(date_path: Path) -> bool:
         try:
-            datetime.strptime(date_str, "%Y-%m-%d")
+            datetime.strptime(date_path.stem, "%Y-%m-%d")
             return True
         except ValueError:
             return False
 
     next_ = None
-
+    if date_filters is None:
+        date_filters = []
+    date_filters = [Path.is_dir, _validate_date, *date_filters]
+    node_filters = node_filters if node_filters is not None else []
     for date in sorted(
         filter(
-            lambda p: p.is_dir() and _validate_date(p.stem), base_path.iterdir()
+            lambda p: all(filter_(p) for filter_ in date_filters),
+            base_path.iterdir(),
         ),
         reverse=True,
     ):
         nodes = date.glob("#*")
-        for node in sorted(nodes, reverse=True):
+        for node in sorted(
+            filter(
+                lambda p: all(filter_(p) for filter_ in node_filters),
+                nodes,
+            ),
+            reverse=True,
+        ):
             current = next_
             if current is None:
                 next_ = id_from_node_name(node.stem)
