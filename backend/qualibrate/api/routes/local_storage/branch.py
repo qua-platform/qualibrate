0e76d5c052543af73c1f18e5d286a362e11cb5ba
Maxim V4S <maxim.d@v4scale.com>
2024-03-25 13:07:09 +0300

    feat: local branch get nodes and snapshots

--- backend/qualibrate/api/routes/local_storage/branch.py
+++ backend/qualibrate/api/routes/local_storage/branch.py
@@ -1,8 +1,12 @@
-from typing import Annotated, Optional
+from typing import Annotated, Optional, cast
 
 from fastapi import APIRouter, Depends, Path
 
+from qualibrate.api.core.bases.branch import BranchLoadType
+from qualibrate.api.core.bases.node import NodeLoadType
+from qualibrate.api.core.bases.snapshot import SnapshotLoadType
 from qualibrate.api.core.local_storage.branch import BranchLocalStorage
+from qualibrate.api.core.local_storage.node import NodeLocalStorage
 from qualibrate.api.core.types import DocumentSequenceType, DocumentType
 
 local_storage_branch_router = APIRouter(
@@ -21,6 +25,44 @@ def get(
 ) -> Optional[DocumentType]:
     branch.load(load_type)
     return branch.content
+
+
+@local_storage_branch_router.get("/snapshot")
+def get_snapshot(
+    branch: Annotated[BranchLocalStorage, Depends(_get_branch_instance)],
+    snapshot_id: int,
+) -> Optional[DocumentType]:
+    snapshot = branch.get_snapshot(snapshot_id)
+    snapshot.load(SnapshotLoadType.Metadata)
+    return snapshot.content
+
+
+@local_storage_branch_router.get("/snapshot/latest")
+def get_latest_snapshot(
+    branch: Annotated[BranchLocalStorage, Depends(_get_branch_instance)],
+) -> Optional[DocumentType]:
+    snapshot = branch.get_snapshot()
+    snapshot.load(SnapshotLoadType.Metadata)
+    return snapshot.content
+
+
+@local_storage_branch_router.get("/node")
+def get_node(
+    branch: Annotated[BranchLocalStorage, Depends(_get_branch_instance)],
+    node_id: int,
+) -> Optional[DocumentType]:
+    node = cast(NodeLocalStorage, branch.get_node(node_id))
+    node.load(NodeLoadType.Full)
+    return node.dump()
+
+
+@local_storage_branch_router.get("/node/latest")
+def get_latest_node(
+    branch: Annotated[BranchLocalStorage, Depends(_get_branch_instance)],
+) -> Optional[DocumentType]:
+    node = cast(NodeLocalStorage, branch.get_node())
+    node.load(NodeLoadType.Full)
+    return node.dump()
 
 
 @local_storage_branch_router.get("/snapshots_history")
