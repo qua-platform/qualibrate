6b6f98a424c0b3028c57eacef159027e44bd0011
Maxim V4S <maxim.d@v4scale.com>
2024-04-17 11:11:19 +0300

    refactor: move common logic to base node class

--- backend/qualibrate/api/core/domain/timeline_db/node.py
+++ backend/qualibrate/api/core/domain/timeline_db/node.py
@@ -1,8 +1,6 @@
-from typing import Optional, cast
+from typing import Optional
 
-from qualibrate.api.core.domain.bases.node import NodeBase, NodeLoadType
-from qualibrate.api.core.domain.bases.snapshot import SnapshotLoadType
-from qualibrate.api.core.domain.bases.storage import DataFileStorage
+from qualibrate.api.core.domain.bases.node import NodeBase
 from qualibrate.api.core.domain.timeline_db.snapshot import SnapshotTimelineDb
 from qualibrate.api.core.types import DocumentType, IdType
 
@@ -14,24 +12,3 @@ class NodeTimelineDb(NodeBase):
         self, node_id: IdType, snapshot_content: Optional[DocumentType] = None
     ):
         super().__init__(node_id, SnapshotTimelineDb(node_id, snapshot_content))
-        self._storage: Optional[DataFileStorage] = None
-
-    @property
-    def load_type(self) -> NodeLoadType:
-        return self._load_type
-
-    def load(self, load_type: NodeLoadType) -> None:
-        if self._load_type == NodeLoadType.Full:
-            return
-        self._snapshot.load(SnapshotLoadType.Metadata)
-        if load_type < NodeLoadType.Full:
-            return
-        self._fill_storage()
-
-    @property
-    def snapshot(self) -> Optional[SnapshotTimelineDb]:
-        return cast(SnapshotTimelineDb, self._snapshot)
-
-    @property
-    def storage(self) -> Optional[DataFileStorage]:
-        return self._storage
