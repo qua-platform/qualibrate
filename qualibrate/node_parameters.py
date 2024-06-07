a0eac60502817bda92f3ea61e39e5612ad510f8e
Maxim V4S <maxim.d@v4scale.com>
2024-06-07 17:25:08 +0300

    feat: node parameters serialize

--- qualibrate/node_parameters.py
+++ qualibrate/node_parameters.py
@@ -2,6 +2,14 @@ from typing import Any, Mapping
 
 from pydantic import BaseModel
 
+# https://docs.pydantic.dev/1.10/usage/schema/#json-schema-types
+types_mapping = {
+    "string": "str",
+    "boolean": "bool",
+    "number": "float",
+    "integer": "int",
+}
+
 
 class NodeParameters(BaseModel):
     def serialize(self) -> list[Mapping[str, Any]]:
@@ -10,7 +18,7 @@ class NodeParameters(BaseModel):
         return [
             {
                 "name": prop,
-                "param_type": value["type"],
+                "param_type": types_mapping[value["type"]],
                 "initial_value": value.get("default")
             }
             for prop, value in schema["properties"].items()
