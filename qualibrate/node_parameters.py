87aa0351873f56bd5cb6effd399da42c12920d08
Maxim V4S <maxim.d@v4scale.com>
2024-06-07 17:02:14 +0300

    feat: node parameters serialize

--- qualibrate/node_parameters.py
+++ qualibrate/node_parameters.py
@@ -1,6 +1,17 @@
+from typing import Any, Mapping
+
 from pydantic import BaseModel
 
 
 class NodeParameters(BaseModel):
-    def serialize(self):
-        pass
+    def serialize(self) -> list[Mapping[str, Any]]:
+        schema = self.schema()
+        # list type is array
+        return [
+            {
+                "name": prop,
+                "param_type": value["type"],
+                "initial_value": value.get("default")
+            }
+            for prop, value in schema["properties"].items()
+        ]
