8994fb7732f65c7d3d340ff23751a84b60e487ff
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-06-10 08:55:51 +0200

    add tests

---
+++ tests/test_node_parameters/test_node_parameter_serialization.py
@@ -0,0 +1,42 @@
+from qualibrate.node_parameters import NodeParameters
+
+
+def test_parameters_empty_serialization():
+    class Parameters(NodeParameters):
+        pass
+
+    parameters = Parameters()
+    serialized = parameters.serialize()
+
+    assert serialized == []
+
+
+def test_parameters_default_types_serialization():
+    class Parameters(NodeParameters):
+        bool_val: bool = False
+        int_val: int = 0
+        float_val: float = 0.0
+        str_val: str = ""
+
+    parameters = Parameters()
+    serialized = parameters.serialize()
+
+    assert serialized == [
+        {"name": "bool_val", "param_type": "bool", "initial_value": False},
+        {"name": "int_val", "param_type": "int", "initial_value": 0},
+        {"name": "float_val", "param_type": "float", "initial_value": 0.0},
+        {"name": "str_val", "param_type": "str", "initial_value": ""},
+    ]
+
+
+def test_parameters_typed_list_serialization():
+
+    class Parameters(NodeParameters):
+        channels: list[str] = []
+
+    parameters = Parameters()
+    serialized = parameters.serialize()
+
+    assert serialized == [
+        {"name": "channels", "param_type": "list[str]", "initial_value": []}
+    ]
