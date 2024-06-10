222704d60c8c35842965bd1153e53b96541cc5e5
Maxim V4S <maxim.d@v4scale.com>
2024-06-10 14:52:27 +0300

    feat: submit job

--- qualibrate_runner/api/dependencies.py
+++ qualibrate_runner/api/dependencies.py
@@ -1,8 +1,33 @@
 from functools import cache
+from typing import Annotated, Mapping
 
-from qualibrate_runner.config import State
+from fastapi import Depends, HTTPException
+from qualibrate.qualibration_node import QualibrationNode
+
+from qualibrate_runner.config import (
+    QualibrateRunnerSettings,
+    State,
+    get_settings,
+)
 
 
 @cache
 def get_state() -> State:
     return State()
+
+
+@cache
+def get_nodes(
+    settings: Annotated[QualibrateRunnerSettings, Depends(get_settings)],
+) -> Mapping[str, QualibrationNode]:
+    return settings.calibration_nodes_resolver()
+
+
+def get_node(
+    name: str,
+    nodes: Annotated[Mapping[str, QualibrationNode], Depends(get_nodes)],
+) -> QualibrationNode:
+    node = nodes.get(name)
+    if node is None:
+        raise HTTPException(status_code=422, detail=f"Unknown node name {name}")
+    return node
---
+++ qualibrate_runner/core/run_job.py
@@ -0,0 +1,53 @@
+from typing import Any, Mapping, Type
+
+from fastapi import HTTPException, status
+from pydantic import BaseModel, ValidationError
+from qualibrate.qualibration_library import QualibrationLibrary
+from qualibrate.qualibration_node import QualibrationNode
+
+from qualibrate_runner.config import State
+from qualibrate_runner.core.models.last_run import LastRun, RunStatus
+
+
+def validate_input_parameters(
+    node_parameters: Type[BaseModel],
+    passed_parameters: Mapping[str, Any],
+) -> BaseModel:
+    try:
+        return node_parameters.model_validate(passed_parameters)
+    except ValidationError as ex:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=ex.errors()
+        )
+
+
+def run_job(
+    node: QualibrationNode,
+    passed_input_parameters: Mapping[str, Any],
+    state: State,
+) -> None:
+    state.passed_parameters = passed_input_parameters
+    state.last_run = LastRun(
+        name=node.name,
+        status=RunStatus.RUNNING,
+        idx=-1,
+    )
+    try:
+        QualibrationLibrary.active_library.run(
+            node,
+            node.parameters(**passed_input_parameters),
+            persistent=state.persistent,
+        )
+    except Exception:
+        state.last_run.status = RunStatus.ERROR
+        raise
+    else:
+        # node.run_summary() == {
+        #   "status: "finished",
+        #   "name": "my_calibration",
+        #   "idx": 423,
+        #   "state_updates": [...]
+        # }
+        state.last_run.status = RunStatus.FINISHED
+        # TODO: assign `state_updates` and `idx`
+        state.last_run.state_updates = []
