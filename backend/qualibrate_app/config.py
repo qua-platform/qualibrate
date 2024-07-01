16cc1cb86dc5b263881aaddb829dc27502e1de54
Maxim V4S <maxim.d@v4scale.com>
2024-07-01 10:16:53 +0300

    refactor: change name of specific config file

--- backend/qualibrate_app/config.py
+++ backend/qualibrate_app/config.py
@@ -20,7 +20,7 @@ else:
 CONFIG_KEY = "qualibrate_app"
 QUALIBRATE_PATH = Path().home() / ".qualibrate"
 DEFAULT_CONFIG_FILENAME = "config.toml"
-DEFAULT_QUALIBRATE_CONFIG_FILENAME = "qualibrate.toml"
+DEFAULT_QUALIBRATE_CONFIG_FILENAME = "qualibrate_app.toml"
 CONFIG_PATH_ENV_NAME = "QUALIBRATE_APP_CONFIG_FILE"
 
 
