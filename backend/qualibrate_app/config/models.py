b5846f8a8fb2623337fb5ec5a6d1b00a60d786e1
Maxim V4S <maxim.d@v4scale.com>
2024-07-22 11:46:09 +0300

    feat: use `/` in references for any paths (ntpath and posix)

--- backend/qualibrate_app/config/models.py
+++ backend/qualibrate_app/config/models.py
@@ -1,10 +1,13 @@
 from enum import Enum
+from os import sep as os_sep
 from pathlib import Path
 from typing import ClassVar, Optional
 
 from pydantic import DirectoryPath, HttpUrl, field_serializer
 from pydantic_core.core_schema import FieldSerializationInfo
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from qualibrate_app.config.references.resolvers import TEMPLATE_START
 
 
 class StorageType(Enum):
@@ -51,7 +54,20 @@ class QualibrateSettingsBase(BaseSettings):
 class QualibrateSettingsSetup(QualibrateSettingsBase):
     @field_serializer("static_site_files", "user_storage")
     def serialize_path(self, path: Path, _info: FieldSerializationInfo) -> str:
-        return str(path)
+        str_path = str(path)
+        if os_sep == "/":
+            return str_path
+        t_start_idx = str_path.find(TEMPLATE_START)
+        while t_start_idx != -1:
+            t_end_idx = str_path.find("}", t_start_idx)
+            if t_end_idx == -1:
+                return str_path
+            new_ref = str_path[t_start_idx:t_end_idx].replace(os_sep, "/")
+            str_path = str_path.replace(
+                str_path[t_start_idx:t_end_idx], new_ref
+            )
+            t_start_idx = str_path.find(TEMPLATE_START, t_end_idx + 1)
+        return str_path
 
     @field_serializer("storage_type")
     def serialize_storage_type(
