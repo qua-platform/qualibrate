9424c2f84ea07d3a88ed074f86a44c1345599e9b
Maxim V4S <maxim.d@v4scale.com>
2024-08-30 17:25:28 +0300

    fix: add empty node parameters on graph

--- qualibrate/qualibration_graph.py
+++ qualibrate/qualibration_graph.py
@@ -13,6 +13,7 @@ from typing import (
     Tuple,
     Type,
     Union,
+    cast,
 )
 from weakref import ReferenceType
 
@@ -186,6 +187,15 @@ class QualibrationGraph(
             )
         )
 
+    def _get_all_nodes_parameters(
+        self,  nodes_parameters: Mapping[str, Any]
+    ) -> Mapping[str, Any]:
+        nodes_class = self.full_parameters_class.model_fields["nodes"].annotation
+        return {
+            name: nodes_parameters.get(name, {})
+            for name in cast(NodesParameters, nodes_class).model_fields.keys()
+        }
+
     def run(self, **passed_parameters: Any) -> BaseRunSummary:
         """
         :param passed_parameters: Graph parameters. Should contain `nodes` key.
@@ -193,7 +203,9 @@ class QualibrationGraph(
         if self._orchestrator is None:
             raise ValueError("Orchestrator not specified")
         self.cleanup()
-        nodes = passed_parameters.get("nodes", {})
+        nodes = self._get_all_nodes_parameters(
+            passed_parameters.get("nodes", {})
+        )
         self.parameters = self.parameters_class.model_validate(
             passed_parameters
         )
