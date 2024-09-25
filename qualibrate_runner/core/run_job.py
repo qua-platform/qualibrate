cddf1c85b455a95046f048d95c79e75e862a6585
Maxim V4S <maxim.d@v4scale.com>
2024-09-25 15:06:35 +0300

    hotfix: pass correct parameters to node.run

--- qualibrate_runner/core/run_job.py
+++ qualibrate_runner/core/run_job.py
@@ -53,7 +53,7 @@ def run_node(
     idx = -1
     run_error = None
     try:
-        node.run(passed_parameters=passed_input_parameters)
+        node.run(**passed_input_parameters)
         node = QualibrationNode.last_executed_node
     except Exception as ex:
         run_status = RunStatus.ERROR
