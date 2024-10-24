feaa7a7cb43ed6dd69ebbf288347dc10ece9258d
Maxim V4S <maxim.d@v4scale.com>
2024-10-24 16:56:00 +0300

    tests: mock save method on run node from library

--- tests/integration/test_qualibration_library/test_run_qualibration_node.py
+++ tests/integration/test_qualibration_library/test_run_qualibration_node.py
@@ -12,10 +12,7 @@ def test_library_scan(tmp_path):
 def test_run_calibration_node_from_library(tmp_path, mocker):
     folder = Path(__file__).parents[1] / "example_calibration_scripts"
     library = QualibrationLibrary(library_folder=folder)
-    mocker.patch(
-        "qualibrate.storage.local_storage_manager.LocalStorageManager.save"
-    )
+    mocked = mocker.patch("qualibrate.qualibration_node.QualibrationNode.save")
     node = library.nodes["basic_node"]
-    # TODO: Mock save method or need to use non-default path for exporting
-    #  results
     library.run_node("basic_node", node.parameters_class())
+    mocked.assert_called_once()
