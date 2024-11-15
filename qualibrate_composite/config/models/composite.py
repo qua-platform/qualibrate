d9bd322fa8060185aeed14f1dfc86dbdac65aa83
Maxim V4S <maxim.d@v4scale.com>
2024-11-15 15:06:10 +0300

    refactor: use qualibrate config

--- qualibrate_composite/config/models/composite.py
+++ qualibrate_composite/config/models/composite.py
@@ -1,18 +1,25 @@
 from typing import ClassVar, Optional
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import SettingsConfigDict
+from qualibrate_config.models.base.base_referenced_settings import (
+    BaseReferencedSettings,
+)
 
 from qualibrate_composite.config.models.remote_services import (
     QualibrateApp,
     QualibrateRunner,
 )
+from qualibrate_composite.config.vars import (
+    Q_COMPOSITE_SETTINGS_ENV_PREFIX,
+)
 
 __all__ = ["QualibrateSettings"]
 
 
-class QualibrateSettings(BaseSettings):
+class QualibrateSettings(BaseReferencedSettings):
     model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(
         extra="ignore",
+        env_prefix=Q_COMPOSITE_SETTINGS_ENV_PREFIX,
     )
     password: Optional[str] = None
 
--- qualibrate_composite/config/resolvers.py
+++ qualibrate_composite/config/resolvers.py
@@ -4,7 +4,9 @@ from pathlib import Path
 from typing import Annotated
 
 from fastapi import Depends
-from qualibrate_config.file import get_config_file, read_config_file
+from qualibrate_config.file import get_config_file
+from qualibrate_config.resolvers import get_config_model
+from qualibrate_config.storage import STORAGE
 
 from qualibrate_composite.config.models.composite import QualibrateSettings
 from qualibrate_composite.config.vars import (
@@ -29,5 +31,9 @@ def get_config_path() -> Path:
 def get_settings(
     config_path: Annotated[Path, Depends(get_config_path)],
 ) -> QualibrateSettings:
-    config = read_config_file(config_path, solve_references=True)
-    return QualibrateSettings(**(config.get(CONFIG_KEY, {})))
+    return get_config_model(
+        config_path=config_path,
+        config_key=CONFIG_KEY,
+        config_model_class=QualibrateSettings,
+        config=STORAGE,
+    )
