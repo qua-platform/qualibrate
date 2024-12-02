98b4bf1211b93c6a5d12da55b7006d0660b74284
Maxim V4S <maxim.d@v4scale.com>
2024-12-02 15:49:05 +0300

    tests: fix load node empty dump

--- tests/integration/test_qualibration_node/test_load_node.py
+++ tests/integration/test_qualibration_node/test_load_node.py
@@ -1,11 +1,34 @@
+from datetime import datetime
 from pathlib import Path
+
+import pytest
 
 from qualibrate.parameters import NodeParameters
 from qualibrate.qualibration_node import QualibrationNode
 
 
-def test_load_from_id_class_empty(nodes_dumps_dir: Path):
-    loaded_node = QualibrationNode.load_from_id(1, base_path=nodes_dumps_dir)
+@pytest.fixture
+def empty_nodes_dump_folder(tmp_path):
+    nodes_path = tmp_path / "nodes_path"
+    nodes_path.mkdir(exist_ok=True, parents=True)
+    yield nodes_path
+
+
+@pytest.fixture
+def empty_node_dir(empty_nodes_dump_folder):
+    date = empty_nodes_dump_folder / datetime.today().strftime("%Y-%m-%d")
+    time = datetime.now().time().strftime("%H:%M:%S")
+    node = date / f"#1_name_{time}"
+    (date / node).mkdir(exist_ok=True, parents=True)
+    yield node
+
+
+def test_load_from_id_class_empty(
+    empty_nodes_dump_folder: Path, empty_node_dir: Path
+):
+    loaded_node = QualibrationNode.load_from_id(
+        1, base_path=empty_nodes_dump_folder
+    )
     assert loaded_node is not None
     assert isinstance(loaded_node.parameters, NodeParameters)
     assert loaded_node.parameters.model_fields == {}
@@ -14,7 +37,7 @@ def test_load_from_id_class_empty(nodes_dumps_dir: Path):
 
 
 def test_load_from_id_class_filled(nodes_dumps_dir: Path):
-    loaded_node = QualibrationNode.load_from_id(2, base_path=nodes_dumps_dir)
+    loaded_node = QualibrationNode.load_from_id(1, base_path=nodes_dumps_dir)
     assert loaded_node is not None
     assert isinstance(loaded_node.parameters, NodeParameters)
     assert loaded_node.parameters.model_dump() == {
