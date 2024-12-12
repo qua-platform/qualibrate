b2d2be63fee32a2ab4f9e3f476dee6e6d18ea4e3
Aleksandar Manasijevic <manas@quantum-machines.co>
2024-12-12 10:52:44 +0100

    Small fix for node selection

--- frontend/src/modules/GraphLibrary/components/GraphStatus/GraphStatus.tsx
+++ frontend/src/modules/GraphLibrary/components/GraphStatus/GraphStatus.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 // eslint-disable-next-line css-modules/no-unused-class
 import styles from "./GraphStatus.module.scss";
-import { GraphStatusContextProvider, useGraphStatusContext } from "./context/GraphStatusContext";
+import { GraphStatusContextProvider, Measurement, useGraphStatusContext } from "./context/GraphStatusContext";
 import { Results } from "../../../Nodes/components/Results/Results";
 import { MeasurementHistory } from "./components/MeasurementHistory/MeasurementHistory";
 import { MeasurementElementGraph } from "./components/MeasurementElementGraph/MeasurementElementGraph";
@@ -13,14 +13,25 @@ const GraphStatus = () => {
   const heading = "Run calibration graph";
   const { setSelectedItemName } = useSelectionContext();
   const { workflowGraphElements } = useGraphContext();
-  const { allMeasurements, result, diffData, fetchResultsAndDiffData, setResult, setDiffData } = useGraphStatusContext();
+  const { allMeasurements, setAllMeasurements, result, diffData, fetchResultsAndDiffData, setResult, setDiffData, fetchAllMeasurements } =
+    useGraphStatusContext();
 
-  const getMeasurementId = (measurementName: string) => {
-    return allMeasurements?.find((measurement) => measurement.name === measurementName)?.snapshot_idx;
+  const getMeasurementId = (measurementName: string, measurements: Measurement[]) => {
+    return measurements?.find((measurement) => measurement.name === measurementName)?.snapshot_idx;
   };
-  const handleOnCytoscapeNodeClick = (name: string) => {
+
+  const setupAllMeasurements = async () => {
+    if (!allMeasurements || allMeasurements.length === 0) {
+      return await fetchAllMeasurements();
+    }
+    return [];
+  };
+
+  const handleOnCytoscapeNodeClick = async (name: string) => {
+    const temp = await setupAllMeasurements();
+    const measurements = temp && temp.length > 0 ? temp : allMeasurements ?? [];
     setSelectedItemName(undefined);
-    const measurementId = getMeasurementId(name);
+    const measurementId = getMeasurementId(name, measurements);
     if (measurementId) {
       setSelectedItemName(name);
       fetchResultsAndDiffData(measurementId);
