13c974e21732f9cb145b447bd87c472d9b2d93a0
Aleksandar Manasijevic <manas@quantum-machines.co>
2025-01-10 11:57:53 +0100

    Added changes for State Update component

--- frontend/src/modules/Nodes/components/RunningJob/RunningJobParameters.tsx
+++ frontend/src/modules/Nodes/components/RunningJob/RunningJobParameters.tsx
@@ -7,22 +7,22 @@ export const RunningJobParameters: React.FC = () => {
   const { runningNode } = useNodesContext();
   return (
     <div className={styles.parametersWrapper}>
-      {Object.entries(runningNode?.parameters ?? {}).length > 0 && (
-        <>
-          <div className={styles.parameterTitleWrapper}>Parameters</div>
-          <div>
-            {
-              // expanded &&
-              Object.entries(runningNode?.parameters ?? {}).map(([key, parameter]) => (
-                <div key={key} className={styles.parameterValues}>
-                  <div className={styles.parameterLabel}>{parameter.title}:</div>
-                  <div className={styles.parameterValue}>{parameter.default?.toString()}</div>
-                </div>
-              ))
-            }
-          </div>
-        </>
-      )}
+      {/*{Object.entries(runningNode?.parameters ?? {}).length > 0 && (*/}
+      <>
+        <div className={styles.parameterTitleWrapper}>Parameters</div>
+        <div>
+          {
+            // expanded &&
+            Object.entries(runningNode?.parameters ?? {}).map(([key, parameter]) => (
+              <div key={key} className={styles.parameterValues}>
+                <div className={styles.parameterLabel}>{parameter.title}:</div>
+                <div className={styles.parameterValue}>{parameter.default?.toString()}</div>
+              </div>
+            ))
+          }
+        </div>
+      </>
+      {/*)}*/}
     </div>
   );
 };
--- frontend/src/ui-lib/Icons/RightArrowIcon.tsx
+++ frontend/src/ui-lib/Icons/RightArrowIcon.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { IconProps } from "../../common/interfaces/IconProps";
 
-export const RightArrowIcon: React.FunctionComponent<IconProps> = ({ width = 16, height = 16 }) => (
-  <svg width={width} height={height} fill="#ffff" viewBox="0 0 330 330">
+export const RightArrowIcon: React.FunctionComponent<IconProps> = ({ width = 10, height = 10 }) => (
+  <svg width={width} height={height} viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
-      id="XMLID_7_"
-      d="M288.107,116.894c-5.858-5.858-15.355-5.858-21.213,0c-5.858,5.858-5.858,15.355,0,21.213L278.787,150H15
-    c-8.284,0-15,6.716-15,15c0,8.284,6.716,15,15,15h263.787l-11.894,11.893c-5.858,5.858-5.858,15.355,0,21.213
-    c2.929,2.929,6.768,4.394,10.606,4.394c3.839,0,7.678-1.464,10.607-4.394l37.5-37.5c5.858-5.858,5.858-15.355,0-21.213
-    L288.107,116.894z"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M7.29287 3.62553L5.14642 1.47908L5.85353 0.771973L8.85353 3.77197L9.20708 4.12553L8.85353 4.47908L5.85353 7.47908L5.14642 6.77197L7.29287 4.62553H0V3.62553H7.29287Z"
+      fill="#A5ACB6"
     />
   </svg>
 );
