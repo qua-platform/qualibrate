7eae5b23ccc7f9dddda4e26ccf65a6cdd04e401d
Aleksandar Manasijevic <manas@quantum-machines.co>
2025-01-29 20:15:59 +0100

    Added Iframe and ToggleSwitch components and their css modules

---
+++ frontend/src/common/ui-components/common/ToggleSwitch/ToggleSwitch.tsx
@@ -0,0 +1,25 @@
+import styles from "./ToggleSwitch.module.scss";
+
+interface IToggleSwitchProps {
+  title: string;
+  activeTab: string;
+  setActiveTab: (a: string) => void;
+}
+
+const ToggleSwitch = ({ title, activeTab, setActiveTab }: IToggleSwitchProps) => {
+  return (
+    <div className={styles.firstRowWrapper}>
+      <h1 className={styles.title}>{title}</h1>
+      <div className={styles.switchWrapper}>
+        <div className={`${styles.switchOption} ${activeTab === "live" ? styles.active : ""}`} onClick={() => setActiveTab("live")}>
+          Live
+        </div>
+        <div className={`${styles.switchOption} ${activeTab === "final" ? styles.active : ""}`} onClick={() => setActiveTab("final")}>
+          Final
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ToggleSwitch;
