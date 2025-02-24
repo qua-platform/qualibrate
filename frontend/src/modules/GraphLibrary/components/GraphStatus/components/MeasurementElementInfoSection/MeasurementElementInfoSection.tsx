b403f2cf525ec080ef0be402a3dec2dac945fb76
Aleksandar Manasijevic <manas@quantum-machines.co>
2025-02-24 09:41:47 +0100

    Small fixes

--- frontend/src/modules/GraphLibrary/components/GraphStatus/components/MeasurementElementInfoSection/MeasurementElementInfoSection.tsx
+++ frontend/src/modules/GraphLibrary/components/GraphStatus/components/MeasurementElementInfoSection/MeasurementElementInfoSection.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styles from "./MeasurementElementInfoSection.module.scss";
 import { classNames } from "../../../../../../utils/classnames";
 
@@ -12,12 +13,14 @@ interface MeasurementElementStatusInfoAndParametersProps {
 /**
  * Reusable Component for Run Info and Parameters Sections.
  */
-export const MeasurementElementStatusInfoAndParameters: React.FC<MeasurementElementStatusInfoAndParametersProps> = ({ 
-  title, data, filterEmpty = false, className, evenlySpaced = false 
+export const MeasurementElementStatusInfoAndParameters: React.FC<MeasurementElementStatusInfoAndParametersProps> = ({
+  title,
+  data,
+  filterEmpty = false,
+  className,
+  evenlySpaced = false,
 }) => {
-  const filteredData = filterEmpty
-    ? Object.entries(data).filter(([, value]) => value != null && value !== "")
-    : Object.entries(data);
+  const filteredData = filterEmpty ? Object.entries(data).filter(([, value]) => value != null && value !== "") : Object.entries(data);
 
   if (filteredData.length === 0) return null;
 
@@ -25,13 +28,14 @@ export const MeasurementElementStatusInfoAndParameters: React.FC<MeasurementElem
     <div className={className}>
       {title && <div className={styles.sectionTitle}>{title}</div>}
       {/* allowing the runInfo section to evenly space its rows while keeping the parameters section with a normal layout */}
-      <div className={styles.infoContent} style={evenlySpaced ? { height: "100%", display: "flex", flexDirection: "column", justifyContent: "space-evenly" } : {}}>
+      <div
+        className={styles.infoContent}
+        style={evenlySpaced ? { height: "100%", display: "flex", flexDirection: "column", justifyContent: "space-evenly" } : {}}
+      >
         {filteredData.map(([key, value]) => (
           <div className={styles.infoItem} key={key}>
             <div className={styles.label}>{key}:</div>
-            <div className={styles.value}>
-              {Array.isArray(value) ? value.join(", ") : value?.toString() || "N/A"}
-            </div>
+            <div className={styles.value}>{Array.isArray(value) ? value.join(", ") : value?.toString() || "N/A"}</div>
           </div>
         ))}
       </div>
@@ -39,34 +43,28 @@ export const MeasurementElementStatusInfoAndParameters: React.FC<MeasurementElem
   );
 };
 
-
 interface MeasurementElementOutcomesProps {
-    outcomes: Record<string, string>;
-  }
-  
+  outcomes: object;
+}
+
 /**
  * Reusable Component for Outcomes Section.
  */
 export const MeasurementElementOutcomes: React.FC<MeasurementElementOutcomesProps> = ({ outcomes }) => {
-    return (
-      <div className={styles.outcomes}>
-        <h4>Outcomes</h4>
-        <div className={styles.outcomeContainer}>
-          {Object.entries(outcomes).map(([qubit, result]) => {
-            const isSuccess = result === "successful";
-            return (
-              <span
-                key={qubit}
-                className={classNames(styles.outcomeBubble, isSuccess ? styles.success : styles.failure)}
-              >
-                <span className={classNames(styles.qubitLabel, isSuccess ? styles.success : styles.failure)}>
-                  {qubit || "N/A"}
-                </span>
-                <span className={styles.outcomeStatus}>{result}</span>
-              </span>
-            );
-          })}
-        </div>
+  return (
+    <div className={styles.outcomes}>
+      <h4>Outcomes</h4>
+      <div className={styles.outcomeContainer}>
+        {Object.entries(outcomes).map(([qubit, result]) => {
+          const isSuccess = result === "successful";
+          return (
+            <span key={qubit} className={classNames(styles.outcomeBubble, isSuccess ? styles.success : styles.failure)}>
+              <span className={classNames(styles.qubitLabel, isSuccess ? styles.success : styles.failure)}>{qubit || "N/A"}</span>
+              <span className={styles.outcomeStatus}>{result}</span>
+            </span>
+          );
+        })}
       </div>
-    );
+    </div>
+  );
 };
