5a17fb9f1590c520f9a21b7e6c4badb04aaeb40b
kleinwave <brandon.klein@quantum-machines.co>
2025-02-27 12:56:08 +0200

    Isolated run icon svg into callable ui icon and used it in nodeElement

--- frontend/src/modules/Nodes/components/NodeElement/NodeElement.tsx
+++ frontend/src/modules/Nodes/components/NodeElement/NodeElement.tsx
@@ -10,6 +10,7 @@ import { ErrorResponseWrapper } from "../../../common/Error/ErrorResponseWrapper
 import InputField from "../../../../common/ui-components/common/Input/InputField";
 import BlueButton from "../../../../ui-lib/components/Button/BlueButton";
 import { NodesApi } from "../../api/NodesAPI";
+import { RunIcon } from "../../../../ui-lib/Icons/RunIcon";
 
 export interface NodeDTO {
   name: string;
@@ -150,12 +151,7 @@ export const NodeElement: React.FC<{ nodeKey: string; node: NodeDTO }> = ({ node
 
         {!isNodeRunning && node.name === selectedItemName && (
           <BlueButton className={styles.runButton} data-testid="run-button" onClick={handleClick}>
-            <svg className={styles.runButtonIcon} xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 12 13" fill="none">
-              <path
-                d="M10.6579 5.71292C11.3246 6.09782 11.3246 7.06007 10.6579 7.44497L2.28947 12.2765C1.62281 12.6614 0.789476 12.1803 0.789476 11.4105L0.789476 1.74744C0.789476 0.977635 1.62281 0.496511 2.28948 0.881412L10.6579 5.71292Z"
-                fill="white"
-              />
-            </svg>
+            <RunIcon className={styles.runButtonIcon} />
             <span className={styles.runButtonText}>Run</span>
           </BlueButton>
         )}
