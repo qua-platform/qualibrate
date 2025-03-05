8333a1bc434c551048eff2a84fea79b9071e97ee
kleinwave <brandon.klein@quantum-machines.co>
2025-03-05 09:54:42 +0200

    removed unnecessary comments

--- frontend/src/ui-lib/Icons/InfoIcon.tsx
+++ frontend/src/ui-lib/Icons/InfoIcon.tsx
@@ -8,13 +8,8 @@ export const InfoIcon: React.FunctionComponent<IconProps> = ({
   color = ACCENT_COLOR_LIGHT,
 }) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 20 20" fill="none">
-    {/* Outer Circle */}
     <circle cx="10" cy="10" r="9" stroke={color} strokeWidth="1" />
-
-    {/* "i" Icon - Dot */}
     <circle cx="10" cy="6" r="1.2" fill={color} />
-
-    {/* "i" Icon - Line */}
     <path d="M10 9.5V14" stroke={color} strokeWidth="2" strokeLinecap="round" />
   </svg>
 );
