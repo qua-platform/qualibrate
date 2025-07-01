6a28b1ed2f55f3df7a4373c9452718fdfac8e661
kleinwave <brandon.klein@quantum-machines.co>
2025-07-01 15:59:20 +0300

    addressing review comments

--- frontend/src/ui-lib/Icons/CollapseSideMenuIcon.tsx
+++ frontend/src/ui-lib/Icons/CollapseSideMenuIcon.tsx
@@ -14,13 +14,13 @@ const CollapseSideMenuIcon: React.FC = () => {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M9.26278 8L16 15.3441L15.3154 16L7.97648 8L15.3154 0L16 0.655904L9.26278 8Z"
-        fill="#A5ACB6"
+        fill="var(--grey-highlight)"
       />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M1.2863 8L8.02352 15.3441L7.33892 16L0 8L7.33892 0L8.02352 0.655904L1.2863 8Z"
-        fill="#A5ACB6"
+        fill="var(--grey-highlight)"
       />
     </svg>
   );
--- frontend/src/ui-lib/Icons/RightArrowIcon.tsx
+++ frontend/src/ui-lib/Icons/RightArrowIcon.tsx
@@ -7,7 +7,7 @@ export const RightArrowIcon: React.FunctionComponent<IconProps> = ({ width = 10,
       fillRule="evenodd"
       clipRule="evenodd"
       d="M7.29287 3.62553L5.14642 1.47908L5.85353 0.771973L8.85353 3.77197L9.20708 4.12553L8.85353 4.47908L5.85353 7.47908L5.14642 6.77197L7.29287 4.62553H0V3.62553H7.29287Z"
-      fill="#A5ACB6"
+      fill="var(--grey-highlight)"
     />
   </svg>
 );
