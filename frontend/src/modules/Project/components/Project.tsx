ea7f1f6eeb7d38ee707658971abf1e9d5569acf1
kleinwave <brandon.klein@quantum-machines.co>
2025-08-03 11:05:43 +0300

    adding checkmark to active project

--- frontend/src/modules/Project/components/Project.tsx
+++ frontend/src/modules/Project/components/Project.tsx
@@ -7,6 +7,8 @@ import cyKeys from "../../../utils/cyKeys";
 import SelectField from "../../../common/ui-components/common/Input/SelectField";
 import { getColorIndex, createClickHandler } from "../helpers";
 import { colorPalette } from "../constants";
+import ProjectCheckIcon from "../../../ui-lib/Icons/ProjectCheckIcon";
+import { useProjectContext } from "../context/ProjectContext";
 
 const SelectRuntime = <SelectField options={["Localhost"]} onChange={() => {}} />;
 
@@ -20,18 +22,29 @@ interface Props {
 }
 
 const Project = ({ showRuntime = false, isActive = false, onClick, name = "", lastModifiedAt }: Props) => {
+  const { activeProject } = useProjectContext();
+  const isCurrentProject = activeProject?.name === name;
   const handleOnClick = createClickHandler(onClick, name);
   const index = getColorIndex(name || "");
   const projectColor = colorPalette[index];
 
   return (
     <button
-      className={classNames(styles.project, isActive && styles.project_active)}
+    className={classNames(
+      styles.project,
+      isActive && styles.projectActive,
+      isCurrentProject && styles.projectChecked
+    )}
       onClick={handleOnClick}
       data-cy={cyKeys.projects.PROJECT}
     >
       <ProjectInfo name={name} colorIcon={projectColor} date={lastModifiedAt ? new Date(lastModifiedAt) : undefined} />
-      <div className={styles.projectActions}>{showRuntime && SelectRuntime}</div>
+      <div className={styles.projectActions}>
+        <div className={styles.checkWrapper}>
+          {isCurrentProject && <ProjectCheckIcon />}
+        </div>
+        {showRuntime && SelectRuntime}
+      </div>
     </button>
   );
 };
