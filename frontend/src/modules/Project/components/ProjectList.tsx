9b322b4e93c8bc9e1e358c1b978cd5383f95b811
kleinwave <brandon.klein@quantum-machines.co>
2025-08-14 14:53:14 +0300

    small fixes

--- frontend/src/modules/Project/components/ProjectList.tsx
+++ frontend/src/modules/Project/components/ProjectList.tsx
@@ -3,7 +3,7 @@ import { NoItemsIcon } from "../../../ui-lib/Icons/NoItemsIcon";
 import Project from "./Project";
 // eslint-disable-next-line css-modules/no-unused-class
 import styles from "./Project.module.scss";
-import React from "react";
+import React, { useMemo } from "react";
 import { ProjectDTO } from "../ProjectDTO";
 
 interface Props {
@@ -13,6 +13,14 @@ interface Props {
 }
 
 const ProjectList = ({ projects, selectedProject, setSelectedProject }: Props) => {
+  const sortedProjects = useMemo(() => {
+    return [...projects].sort((a, b) => new Date(b.last_modified_at).getTime() - new Date(a.last_modified_at).getTime());
+  }, [projects]);
+
+  const onClickHandler = (project: ProjectDTO) => {
+    setSelectedProject(project);
+  };
+
   if (!projects?.length) {
     return (
       <div className={styles.splash}>
@@ -23,18 +31,16 @@ const ProjectList = ({ projects, selectedProject, setSelectedProject }: Props) =
 
   return (
     <div className={styles.splash}>
-      {[...projects]
-        .sort((a, b) => new Date(b.last_modified_at).getTime() - new Date(a.last_modified_at).getTime())
-        .map((project, index) => (
-          <Project
-            key={index}
-            isActive={selectedProject?.name === project.name}
-            projectId={index}
-            name={project.name}
-            onClick={() => setSelectedProject(project)}
-            lastModifiedAt={project.last_modified_at}
-          />
-        ))}
+      {sortedProjects.map((project, index) => (
+        <Project
+          key={index}
+          isActive={selectedProject?.name === project.name}
+          projectId={index}
+          name={project.name}
+          onClick={() => onClickHandler(project)}
+          lastModifiedAt={project.last_modified_at}
+        />
+      ))}
     </div>
   );
 };
