d290b9cd8ce43c3108cecea58e8afcf044617f77
Maxim V4S <maxim.d@v4scale.com>
2025-09-02 14:12:52 +0300

    feat: process time response header

--- qualibrate_runner/app.py
+++ qualibrate_runner/app.py
@@ -1,11 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 
+from qualibrate_runner.api.middleware.process_time import ProcessTimeMiddleware
 from qualibrate_runner.api.routes import base_router
 from qualibrate_runner.api.sockets import base_ws_router
 from qualibrate_runner.core.app.lifespan import app_lifespan
 
 app = FastAPI(lifespan=app_lifespan)
+app.add_middleware(ProcessTimeMiddleware)
 app.include_router(base_router)
 app.include_router(base_ws_router)
 
