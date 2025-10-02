c945f734f55f602a95a41a7a837f760a850e5152
Jonatan Kutchinsky <jonatan@quantum-machines.co>
2025-10-02 07:19:19 +0200

    Lint fix

--- frontend/src/test-utils/setup.ts
+++ frontend/src/test-utils/setup.ts
@@ -35,6 +35,6 @@ global.ResizeObserver = vi.fn(() => ({
 })) as any;
 
 // Setup MSW server
-beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }));
+beforeAll(() => server.listen({ onUnhandledRequest: "warn" }));
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
