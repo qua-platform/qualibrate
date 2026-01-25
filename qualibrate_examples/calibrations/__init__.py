0694f5a7a775ccf192bcbd7c13a330982840efa6
Elad-Zaharan <elad.zaharan@quantum-machines.co>
2026-01-25 18:17:45 +0200

    moved examples into src

--- qualibrate_examples/calibrations/__init__.py
+++
@@ -1,5 +0,0 @@
-"""QUAlibrate demo calibrations.
-
-Hardware-free demo calibrations for new users to experience QUAlibrate's
-capabilities without requiring actual quantum hardware.
-"""
--- qualibrate_examples/scripts/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Scripts for QUAlibrate examples."""
